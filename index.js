33c28da9fe1398bfa41a958769014801fd1aba5b
Yuvraj Sood <soodyuvrajo4@gmail.com>
2022-11-03 17:26:56 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+app.get('/', (req,res)=> {
+    res.json({
+        hello: 'hi!',
+    }) ;
+
+}) ;
+
+
+
+
+
+app.listen("3000", ()=> {
+    console.log('Server is listening to Port 3000') ;
+}) ;
